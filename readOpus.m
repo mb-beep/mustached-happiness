9bd4627a50623aefd3dbf7d4e43b8a38997df681
munirbe <munir.b@me.com>
2014-12-01 19:00:11 +0100

    read script

---
+++ readOpus.m
@@ -0,0 +1,20 @@
+% read spectral data from .mat files which were collected with bruker's
+%  opus software using one of the bruker devices
+% 01.12.2014
+%%
+path = uigetdir();
+content = dir(path);
+absorbance = [];
+for i = 1:length(content)
+    tmp = content(i);
+    if(~tmp.isdir && ~isempty(strfind(tmp.name, '.mat')))
+        tmpPath = [path filesep tmp.name];
+        load(content(i).name, 'AB');
+        wn = AB(:,1);
+        absorbance = [absorbance AB(:,2)];
+    end
+end
+plot(wn,absorbance);
+xlabel('Wavenumbers [cm-1]');
+ylabel('Absorbance');
+axis([min(wn) max(wn) min(min(absorbance)) max(max(absorbance))]);
