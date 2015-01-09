24375e398d27f1977e0b9ff4235c12278a9c6411
munirbe <munir.b@me.com>
2015-01-09 14:39:29 +0100

    fixed heights in acAc.m

--- dataFromP/acAc.m
+++ dataFromP/acAc.m
@@ -5,9 +5,9 @@
 spcE = spcO(:, 650:750);
 wn = 200:1:2000;
 wnE = wn(650:750);
-[bldata,wts,baseline,vweights] = wlsbaseline(y_hat,5);
+[bldata,wts,baseline,vweights] = wlsbaseline(spcE,4);
 plot(wnE, bldata', '-', 'color', 'r');
-%[y_hat, cm] = savgol(bldata,9,5,0);
+[y_hat, cm] = savgol(bldata,9,2,0);
 wnEint = min(wnE):0.1:max(wnE);
 ySpline = spline(wnE,y_hat,wnEint);
 hold on;
@@ -21,8 +21,11 @@ plot(wnP1, peak1);
 figure();
 wnP2 = wnEint(600:1000);
 plot(wnP2, peak2);
-[maxP1, indP1] = max(peak1(:,1));
-[maxP2, indP2] = max(peak2(:,end));
+%%
+[maxP1, maxInd1] = max(max(peak1));
+[maxP2, maxInd2] = max(max(peak2));
+[maxP1, indP1] = max(peak1(:,maxInd1));
+[maxP2, indP2] = max(peak2(:,maxInd2));
 indP1D = wnP1(indP1);
 indP2D = wnP2(indP2);
 
@@ -33,3 +36,8 @@ heights2 = peak2(indP2,:);
 % round to be able to get right position
 maxPeak1 = round(maxPeak1 * 10) / 10;
 maxPeak2 = round(maxPeak2 * 10) /10;
+
+% plot line through heights
+hold on
+plot([indP2D indP2D], [0 maxP2]);
+hold off
